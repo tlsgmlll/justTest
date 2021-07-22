b0c2cffd20525d196197f191988682e9c0f0959b
HOME <HOME@DESKTOP-MLQ4HAU>
2021-07-22 12:04:45 +0900

    2021072212_코드를 받은 조원으로써 첫번째 커밋

--- src/com/greedy/git/Application.java
+++ src/com/greedy/git/Application.java
@@ -7,6 +7,7 @@ public class Application {
 		System.out.println("오~ 깃허브에 뭔가 올라가는데 ???");
 		System.out.println("한줄 추가하기");
 		System.out.println("또 한줄 추가");
+		System.out.println("나도 나름 코딩 열심히 하고 있어");
 	}
 
 }
