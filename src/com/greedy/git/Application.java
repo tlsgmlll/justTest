7e187374b67b2f947a384b5b435d7008609cbc17
HOME <HOME@DESKTOP-MLQ4HAU>
2021-07-22 12:05:41 +0900

    2021072212_나만 혼자 코딩하나보다

--- src/com/greedy/git/Application.java
+++ src/com/greedy/git/Application.java
@@ -7,6 +7,7 @@ public class Application {
 		System.out.println("오~ 깃허브에 뭔가 올라가는데 ???");
 		System.out.println("한줄 추가하기");
 		System.out.println("또 한줄 추가");
+		System.out.println("난 원래 계속 하고 있어");
 	}
 
 }
